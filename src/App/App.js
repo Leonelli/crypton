fd46fdee62cb54ae1febc07b53e86f26854bb645
Matteo Paoli <matteo_paoli@outlook.com>
2020-03-28 05:10:34 +0100

    Standard JS refactor

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import Container from '../Container/Container';
-import './App.css';
-import Sidebar from '../Sidebar/Sidebar';
+import React from 'react'
+import Container from '../Container/Container'
+import './App.css'
+import Sidebar from '../Sidebar/Sidebar'
 
 class App extends React.Component {
   state = {
-    active: 'HPQ',
+    active: 'HPQ'
   }
 
-  render() {  
+  render () {
     return (
       <div className="App">
         <Sidebar updater={this.updateActiveStock}/>
         <Container active={this.state.active}/>
       </div>
-    );
+    )
   }
 
   updateActiveStock = (event) => {
-    this.setState({ 
+    this.setState({
       active: event.target.id
-    });
+    })
   }
-
 }
 
-export default App;
+export default App
--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Coin from '../Coin/Coin';
-import ContainerTab from '../ContainerTab/ContainerTab';
-import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
-import './Container.css';
+import React from 'react'
+import Coin from '../Coin/Coin'
+import ContainerTab from '../ContainerTab/ContainerTab'
+import { LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts'
+import PropTypes from 'prop-types'
+import './Container.css'
 
 class Container extends React.Component {
-
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       nameStocks: props.active,
       valueStocks: null,
@@ -17,69 +17,71 @@ class Container extends React.Component {
     }
   }
 
-
   fetchInfo = () => {
-      fetch(`https://financialmodelingprep.com/api/v3/stock/real-time-price/${this.props.active}`)
+    fetch(`https://financialmodelingprep.com/api/v3/stock/real-time-price/${this.props.active}`)
       .then(res => res.json())
       .then(
-        result => { this.setState({ isLoaded: false, valueStocks: result.price }); },
-        error => { this.setState({ error }); }
+        result => { this.setState({ isLoaded: false, valueStocks: result.price }) },
+        error => { this.setState({ error }) }
       )
-      fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.props.active}?serietype=line`)
+    fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${this.props.active}?serietype=line`)
       .then(res => res.json())
       .then(
         result => {
-          const data = result.historical.map(x => ({ name: x.date, value: x.close }));
-          let min = +Infinity;
-          let max = -Infinity;
+          const data = result.historical.map(x => ({ name: x.date, value: x.close }))
+          let min = +Infinity
+          let max = -Infinity
           data.forEach(element => {
             if (element.value < min) {
-              min = element.value;
+              min = element.value
             }
             if (element.value > max) {
-              max = element.value;
+              max = element.value
             }
-          });
-          this.setState({ 
+          })
+          this.setState({
             historicValue: data,
             minValue: min,
-            maxValue: max,
+            maxValue: max
           })
         },
-        error => { this.setState({ error }); }
+        error => { this.setState({ error }) }
       )
-    }
+  }
 
-  componentDidMount() {
-    this.fetchInfo();
+  componentDidMount () {
+    this.fetchInfo()
     // this.setState({
     //   isLoaded: true,
     // })
   }
 
-  componentDidUpdate() {
-    this.fetchInfo();
+  componentDidUpdate (prevState, prevProps) {
+    if (prevState) {
+      this.fetchInfo()
+    }
   }
 
   containerTabClickHandler = (stocks) => {
-    this.setState({nameStocks: stocks,
-      isLoaded: true,
+    this.setState({
+      nameStocks: stocks,
+      isLoaded: true
     })
   }
-  
-  render() {
+
+  render () {
     return (
       <div className="container">
         <div className="container__tabs">
-          <ContainerTab onClick={() => this.containerTabClickHandler("AAPL")}>Apple</ContainerTab>
-          <ContainerTab onClick={() => this.containerTabClickHandler("FCAU")}>Fiat</ContainerTab>
-          <ContainerTab onClick={() => this.containerTabClickHandler("GM")}>General Motors</ContainerTab>
+          <ContainerTab onClick={() => this.containerTabClickHandler('AAPL')}>Apple</ContainerTab>
+          <ContainerTab onClick={() => this.containerTabClickHandler('FCAU')}>Fiat</ContainerTab>
+          <ContainerTab onClick={() => this.containerTabClickHandler('GM')}>General Motors</ContainerTab>
         </div>
         <div className="container__main">
-          <div className="narrow"> 
-              <Coin imageURL={this.state.nameStocks+".svg"}/>
+          <div className="narrow">
+            <Coin imageURL={`${this.state.nameStocks}.svg`}/>
           </div>
-          <div className="wide"> 
+          <div className="wide">
             <div className="valuecoin">
               <p className="valuecoin__value">Current value: {this.state.valueStocks}</p>
               <p className="valuecoin__value">Min value: {this.state.minValue}</p>
@@ -96,12 +98,14 @@ class Container extends React.Component {
           <Tooltip />
           <Legend />
           <Line type="monotone" dataKey="value" stroke="#8884d8" dot={false} />
-      </LineChart>
+        </LineChart>
       </div>
-      );
+    )
   }
 }
 
-export default Container;
+export default Container
 
-//https://financialmodelingprep.com/developer/docs/#Stock-Price
+Container.propTypes = {
+  active: PropTypes.string
+}
